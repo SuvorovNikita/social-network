292b610f5ef13404c3433fa5c25b4263b7756e63
SuvorovNikita <nikit.suvorv@gmail.com>
2020-08-19 15:20:22 +0300

    разбил на компоненты

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://images6.alphacoders.com/896/896154.png" alt="Error"/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div><img src="https://pm1.narvii.com/6911/d76ea35645ae06a9fe61855ed6bfa59ee5d07b45r4-736-368_00.jpg" alt=""/></div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>New posts</div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
