b4b13d4e87e0871934c0aaba7215d1ffddabfc49
SuvorovNikita <nikit.suvorv@gmail.com>
2020-08-24 12:11:05 +0300

    add Navlink

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 
 import classes from "./Nav.module.scss";
+import {NavLink} from "react-router-dom";
 
 
 const Nav = () => {
     return <nav className={classes.nav}>
-        <a className={`${classes.item} ${classes.active}`} href="/profile">Profile</a>
-        <a className={classes.item} href="/dialogs">Messages</a>
-        <a className={classes.item} href="/news"> News</a>
-        <a className={classes.item} href="/music"> Music</a>
-        <a className={classes.item} href="settings">Settings</a>
+        <NavLink className={classes.item} activeClassName={classes.active} to="/profile">Profile</NavLink>
+        <NavLink className={classes.item} activeClassName={classes.active} to="/dialogs">Messages</NavLink>
+        <NavLink className={classes.item} activeClassName={classes.active} to="/news"> News</NavLink>
+        <NavLink className={classes.item} activeClassName={classes.active} to="/music"> Music</NavLink>
+        <NavLink className={classes.item} activeClassName={classes.active} to="settings">Settings</NavLink>
     </nav>
 }
 
