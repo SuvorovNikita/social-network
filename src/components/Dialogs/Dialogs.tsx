9584192ac9ad351b7a9499b174a00b3b7941d9b0
SuvorovNikita <nikit.suvorv@gmail.com>
2020-08-24 12:39:34 +0300

    верстка dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,7 +3,33 @@ import React from "react";
 import classes from "./Dialogs.module.scss";
 
 const Dialogs = () => {
-    return <div>Dialogs</div>
+    return <div className={classes.dialogs}>
+
+        <div className={classes.dialogsItem}>
+            <div className={classes.dialog + ' ' + classes.active}>
+                Tobi
+            </div>
+            <div className={classes.dialog}>
+                Pain
+            </div>
+            <div className={classes.dialog}>
+                Conan
+            </div>
+            <div className={classes.dialog}>
+                Itachi
+            </div>
+            <div className={classes.dialog}>
+                Hidan
+            </div>
+        </div>
+
+        <div className={classes.messages}>
+            <div className={classes.message}>Hi</div>
+            <div className={classes.message}>asdasd</div>
+            <div className={classes.message}>gdsggfsdg</div>
+        </div>
+
+    </div>
 }
 
 export default Dialogs;
