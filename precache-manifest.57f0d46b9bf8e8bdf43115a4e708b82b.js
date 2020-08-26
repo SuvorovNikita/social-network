e07c58aeb7558dcefb17dbcb4f39d98722b22c93
SuvorovNikita <nikit.suvorv@gmail.com>
2020-08-26 17:01:59 +0300

    Updates

---
+++ precache-manifest.57f0d46b9bf8e8bdf43115a4e708b82b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6623101c9b849075062b31780c1004f",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "6577339667e15209d42d",
+    "url": "/social-network/static/css/main.8ae8f5c6.chunk.css"
+  },
+  {
+    "revision": "b51f23e7162c1c0b0395",
+    "url": "/social-network/static/js/2.584ec83d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.584ec83d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6577339667e15209d42d",
+    "url": "/social-network/static/js/main.0792548b.chunk.js"
+  },
+  {
+    "revision": "73dc02b1c42065a1d3d3",
+    "url": "/social-network/static/js/runtime-main.a948e851.js"
+  },
+  {
+    "revision": "e6d9ba3676c52d6c974da88d5e786f73",
+    "url": "/social-network/static/media/logo.e6d9ba36.png"
+  }
+]);
